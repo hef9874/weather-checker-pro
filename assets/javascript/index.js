0635b420a70048aa6299f2f99ff9b102eae0f986
nah <hef9874@gmail.com>
2023-01-22 15:28:03 -0500

    Add .js for search button and notes

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,1 +1,28 @@
 var apiKey = "8cb12ab65dcc090cdebeed935aba8314";
+
+$(function(){
+    getSearchHistory();
+    var btnSearch = $('.submit');
+    txtSearch = $('.search');
+
+ //Search Button to find city
+    btnSearch.on('click', searchResults);
+    textSearch.on('keyup', (event) => {
+        if (event.key === 'Enter') {
+            displaySearch();
+        }
+    })
+)};
+
+searchResults(() => {
+//get today's date
+
+
+//fetch(queryCity) for city lat and lon to pull weather 
+
+
+//put in city coords and get weather
+//'https://api.openweathermap.org/data/2.5/forecast?lat=' + data[0].lat + '&lon=' + data[0].lon + '&units=imperial&appid=' + APIKey;
+
+
+})
